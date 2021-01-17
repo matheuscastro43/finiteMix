8971fa49623d273f99273ab13c68244740a7bb9a
Matheus Castro <mtcastro43@gmail.com>
2021-01-17 14:16:54 -0300

    Better quantilic function to Generalized Lindley

--- R/qglindley.R
+++ R/qglindley.R
@@ -1,23 +1,39 @@
-qglindley <- function(p, alpha, beta, gamma, lower.tail = TRUE){
-  if(length(p) == 1){
-
-    if(min(c(alpha, beta, gamma)) > 0){
-      if(lower.tail == FALSE){p = 1 - p}
-      if(p < 0 || p > 1){return(NaN)}
-      else{
-        if(p == 0){return(0)}
-        else{
-          if(p == 1){return(Inf)}
-          else{
-            h <- function(q){abs(pglindley(q, alpha, beta, gamma) - p)}
-            return(optim(par = moglindley(alpha, beta, gamma), h, method = "L-BFGS-B")$par)
+qglindley <- function (p, alpha, beta, gamma, lower.tail = TRUE){
+  if (length(p) == 1) {
+    if (min(c(alpha, beta, gamma)) > 0) {
+      if (lower.tail == FALSE) {
+        p = 1 - p
+      }
+      if (p < 0 || p > 1) {
+        return(NaN)
+      }
+      else {
+        if (p == 0) {
+          return(0)
+        }
+        else {
+          if (p == 1) {
+            return(Inf)
+          }
+          else {
+            U = aux = 100
+            h = function(q){
+              pglindley(q, alpha, beta, gamma) - p
+            }
+            while(aux >= 0.9*U){
+              U = 2*U
+              aux = uniroot(h, lower = 0, upper = U)$root
+            }
+            return(aux)
           }
         }
       }
     }
-    
-  }else{
-    j <- function(p){qglindley(p, alpha, beta, gamma, lower.tail)}
+  }
+  else {
+    j = function(p) {
+      qglindley(p, alpha, beta, gamma, lower.tail)
+    }
     return(sapply(p, j))
   }
 }
