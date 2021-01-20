8bd2289ba50bcc9eca8411ba07d6ff430db0dfd5
Matheus Castro <mtcastro43@gmail.com>
2021-01-19 22:22:22 -0300

    manual additions.

--- R/pbetar.R
+++ R/pbetar.R
@@ -3,7 +3,11 @@ pbetar <- function(q, pi, mu, phi, lower.tail = TRUE, log.p = FALSE){
     g = length(pi)
     if(sum(pi) == 1 && min(c(pi, mu, phi)) > 0 && length(mu) == 1 && length(phi) == 1 &&
        length(pi) == 2 && mu < 1){
-      aux = pi[1] * q * (q>=0 && q<=1) + pi[1] * (q > 1) + pi[2]*pbeta(q, mu*phi, (1-mu)*phi + 1)
+      if(q == Inf){
+        aux = 1
+      }else{
+        aux = pi[1] * q * (q>=0 && q<=1) + pi[1] * (q > 1) + pi[2]*pbeta(q, mu*phi, (1-mu)*phi + 1)
+      }
       if(!lower.tail){
         aux = 1 - aux
       }
