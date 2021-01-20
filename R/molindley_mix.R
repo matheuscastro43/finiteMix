e2d9524edf3daef56665e8fa4dd416bb6630efc7
Matheus Castro <mtcastro43@gmail.com>
2021-01-19 23:15:35 -0300

    manual additions.

--- R/molindley_mix.R
+++ R/molindley_mix.R
@@ -1,5 +1,5 @@
 molindley_mix <- function(pi, beta){
-  if(beta > 0 && (length(pi) == length(beta))){
+  if(min(beta) > 0 && (length(pi) == length(beta))){
     modes <- sapply(beta, molindley)
     return(modes[dlindley_mix(modes, pi, beta) == max(dlindley_mix(modes, pi, beta))])
   }
