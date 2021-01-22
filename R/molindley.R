dc5e7d23521121de3779936fd70ecd170a0c4d47
Matheus Castro <you@example.com>
2021-01-22 11:30:38 -0300

    Perfomance Improvements.

--- R/molindley.R
+++
@@ -1,7 +0,0 @@
-molindley <- function(beta){
-  if(beta > 1)
-    return(beta - 1)
-  if(beta > 0)
-    return(0)
-  cat("Error.")
-}
--- R/molindley_mix.R
+++
@@ -1,7 +0,0 @@
-molindley_mix <- function(pi, beta){
-  if(min(beta) > 0 && (length(pi) == length(beta))){
-    modes <- sapply(beta, molindley)
-    return(modes[dlindley_mix(modes, pi, beta) == max(dlindley_mix(modes, pi, beta))])
-  }
-  cat("Error.")
-}
