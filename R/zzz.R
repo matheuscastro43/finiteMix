fb7b4b2b2d8e421f389625ab531425b26d51472f
Matheus Castro <you@example.com>
2021-03-13 17:31:33 -0300

    Grammar correction.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname){
-  packageStartupMessage("You has loaded finiteMix library")
+  packageStartupMessage("You have loaded finiteMix library")
   packageStartupMessage("By Matheus Oliveira de Castro <mtcastro43@gmail.com>")
   packageStartupMessage("and Gualberto Agamez Montalvo") 
 }
