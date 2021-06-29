48e77ec31ba861a125ed2a3af96a64a97d52353d
Matheus Castro <mtcastro43@gmail.com>
2021-06-29 18:19:01 -0300

    Correction on information criterions

--- R/enorm_mix.R
+++ R/enorm_mix.R
@@ -75,7 +75,7 @@ enorm_mix = function(data, g, lim.em = 100, criteria = "dif.psi",
         break
       }
     }
-    p = prod(dim(matrix(0, 2, 2))) - 1
+    p = 3*g - 1
     aic = 2*p - 2*LF_new
     bic = p*log(n) - 2*LF_new
     if(plot.it){
